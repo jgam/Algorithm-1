a1597b319ccafd594b8f818a682c5351c2fa8dbc
byeong07 <byeong9935@naver.com>
2019-12-01 02:35:15 +0900

    ddddddddddd

---
+++ AlgorithmAn/lowerbound.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+long long int build[1000001];
+long long int tot, last;
+
+bool cmp(int a, int b)
+{
+	if (a > b)
+	{
+		return true;
+	}
+	return false;
+}
+
+int main(void)
+{
+	int i, n;
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++)
+	{
+		long long int h;
+		scanf("%d", &h);
+		last = (int)(lower_bound(build, build + last + 1, h, cmp) - build);
+		tot += last;
+		build[last] = h;
+	}
+
+	printf("%lld\n", tot);
+	return 0;
+}
