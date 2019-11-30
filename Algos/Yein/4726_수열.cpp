20716ffbb1f6e696150c8e69202a8521265062af
yein9 <kooyeah324@gmail.com>
2019-11-30 16:26:45 +0900

    11.24

---
+++ Algos/Yein/4726_수열.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+	vector<int>v;
+	int tc,n,days,max=-9999999;
+	cin >> tc;
+	cin >> days;
+	for (int i = 0; i < tc; i++) {
+		cin >> n;
+		v.push_back(n);
+	}
+	int sum;
+	for (int i = 0; i <= tc - days; i++) {
+		sum = 0;
+		for (int j = i,cnt=0; cnt < days; j++,cnt++) {
+			sum += v[j];
+		}
+		if (sum > max)max = sum;
+	}
+	cout << max;
+	
+}
